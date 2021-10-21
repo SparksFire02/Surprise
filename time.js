331a752a49ff82c32024c345d068b909e5a52391
Spectra <sparksfiredefault@gmail.com>
2021-10-21 12:49:16 +0800

    stages

--- time.js
+++ time.js
@@ -1,5 +1,6 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 1, 2021 12:00:00").getTime();
+var countDownDate = new Date("Nov 1, 2021 00:00:00").getTime();
+var countDownDate2 = new Date("Nov 1, 2021 12:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -47,3 +48,49 @@ var x = setInterval(function() {
     document.getElementById("time").innerHTML = "Released!";
   }
 }, 1000);
+
+var y = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+      
+    // Find the distance between now and the count down date
+    var distance = countDownDate2 - now;
+      
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+      
+    // Output the result in an element with id="demo"
+      if(days <= 1)
+          var dayString = "day"
+      else
+          var dayString = "days"
+  
+      if(hours <= 1)
+          var hrString = "hour"
+      else
+          var hrString = "hours"
+      
+      if(minutes <= 1)
+          var minString = "min"
+      else
+          var minString = "mins"
+  
+      if(seconds <= 1)
+          var secString = "sec"
+      else
+          var secString = "secs"
+  
+  
+    document.getElementById("time2").innerHTML = days + " " + dayString + " " + hours + " " + hrString + " "
+    + minutes + " " + minString + " " + seconds + " " + secString;
+      
+    // If the count down is over, write some text 
+    if (distance < 0) {
+      clearInterval(y);
+      document.getElementById("time2").innerHTML = "Released!";
+    }
+  }, 1000);
